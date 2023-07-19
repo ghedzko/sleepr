d8da64dc370380c2c840086e4413a9ed0595d4af
Guillermo Hedzko <ghedzko@gmail.com>
2023-07-19 15:52:13 -0300

    feat: add config module

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'process';
+import { ConfigModule } from '../config/config.module';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
 export class DatabaseModule {}
