79c3db8efc97e9efced17f13528d7d171e617ea9
Guillermo Hedzko <ghedzko@gmail.com>
2023-07-27 15:18:30 -0300

    feat: add auth app & users module

---
+++ apps/auth/src/users/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsStrongPassword()
+  @IsNotEmpty()
+  readonly password: string;
+}
