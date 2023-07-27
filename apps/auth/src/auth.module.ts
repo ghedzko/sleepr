93889448578698e8d413af833ee78e860e66bcee
Guillermo Hedzko <ghedzko@gmail.com>
2023-07-27 16:23:06 -0300

    feat: config module relocation

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
+import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    LoggerModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: `${configService.get('JWT_EXPIRATION')}` },
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 import { config } from 'process';
-import { ConfigModule } from '../config/config.module';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         uri: configService.get('MONGODB_URI'),
       }),
